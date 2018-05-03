561745288080684223c5a435988cf5cda39da945
Boury Elie <elie.boury@etu.u-bordeaux.fr>
2018-05-03 21:17:11 +0200

    code cleaning

--- src/Game.java
+++ src/Game.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 public class Game {
 
+    private String wordRead;
+
     /**
      * Main loop
      *
@@ -15,128 +17,29 @@ public class Game {
     /**
      * Loop of the game
      */
-    public static void launch() {
+    static void launch() {
+
         boolean endGame = false;
-        boolean endRoom;
-
-        ArrayList<Item> inventoryNull = new ArrayList<>();
-
-        Character player = new Character(
-                "AVAST", false, "C'est vous !", true, 1, 6, 1, inventoryNull);
-        Character perso1 = new Character(
-                "Antivira", false, "Antivira est un anti virus", false, 1, 6, 1, inventoryNull);
-        Character perso2 = new Character(
-                "JigSaw", true, "JigSaw est un virus méchant", false, 1, 4, 1, inventoryNull);
-
-        Item item1 = new Item(
-                "Bleuvrage", -2, 0, true, false, "Cet objet augmente la limite " +
-                "max des dès de 2 pour toujours !");
-        Item item2 = new Item(
-                "Gourde mystère", 0, -2, true, false, "Cet objet diminue la limite " +
-                "max des dès de 2 pour toujours !");
-
         ArrayList<Room> rooms = new ArrayList<>();
-        Room room1 = new Room(0, "Salle initiale", "Salle de préparation");
-        room1.getItems().add(item1);
-        room1.getItems().add(item2);
-        room1.getCharacters().add(perso1);
-        room1.getCharacters().add(perso2);
-        room1.getLockedCharacters().add(perso2);
-        rooms.add(room1);
+        ArrayList<Character> characters = new ArrayList<>();
+        ArrayList<Item> items = new ArrayList<>();
 
-        Room room2 = new Room(1, "Salle infernale", "Description salle infernale");
-        rooms.add(room2);
-
-        player.setCurrentRoom(rooms.get(0));
+        initialize(rooms, characters, items);
 
         //Start of the game
         System.out.println("ERREUR ERREUR, VOTRE ORDINATEUR A ETE INFECTE !\n" +
                 "Passez de salle en salle et tuez les virus grâce à des combats de dés !\n" +
                 "Bon courage, mais faites vite !\n" +
                 "-------------------------------------");
+        System.out.println("Vous êtes dans la " + characters.get(0).getCurrentRoom().getName() + ".\n" +
+                "Essayez d'en sortir !\n");
+        System.out.println("Pour ramasser un objet, tapez \"take\".\n" +
+                "Pour parler à un personnage, tapez \"speak\".\n" +
+                "Pour connaître d'autres commandes, tapez \"help\".");
 
         //Start of the game loop
         while (!endGame) {
-            System.out.println("Vous êtes dans la " + player.getCurrentRoom().getName() + ".\n" +
-                    "Essayez d'en sortir !\n");
-            System.out.println("Pour ramasser un objet, tapez \"take\".\n" +
-                    "Pour parler à un personnage, tapez \"speak\".\n" +
-                    "Pour connaître d'autres commandes, tapez \"help\".");
-            endRoom = false;
-            while (!endRoom) {
-                //Ask of action from the player
-                System.out.println("Agissez !");
-
-                Scanner sc = new Scanner(System.in);
-                String motEntré = sc.nextLine();
-
-                switch (motEntré) {
-                    case ("help"):
-                        help();
-                        break;
-                    case ("quit"):
-                        quit();
-                        break;
-                    case ("inventory"):
-                        player.manageInventory();
-                        break;
-                    case ("speak"):
-                        System.out.println("Avec qui voulez-vous parler ?");
-                        String motEntré2 = sc.nextLine();
-                        if (Room.containCharac(motEntré2, room1.getCharacters())) {
-                            if (Room.getCharac(motEntré2, room1.getCharacters()).isWicked()) {
-                                System.out.println(Room.getCharac(motEntré2, room1.getCharacters()).getName() +
-                                        " n'aime pas quand on lui parle..\n" +
-                                        "Cela va se régler en combat !");
-                                player.fight(Room.getCharac(motEntré2, room1.getCharacters()), 2);
-                            } else {
-                                System.out.println(Room.getCharac(motEntré2, room1.getCharacters()).getName() +
-                                        " veut vous aider mais ne sait toujours pas comment, revenez plus tard !");
-                            }
-                        } else {
-                            System.out.println("Ce personnage n'est pas dans cette salle.");
-                        }
-                        break;
-                    case ("take"):
-                        System.out.println("Quel objet voulez-vous récupérer ?");
-                        String motEntré3 = sc.nextLine();
-                        if (Item.containItem(motEntré3, room1.getItems())) {
-                            player.takeItem(Item.getItem(motEntré3, room1.getItems()));
-                        } else {
-                            System.out.println("Cet item n'est pas dans cette salle.");
-
-                        }
-                        break;
-                    case ("look room"):
-                        player.getCurrentRoom().presentRoom();
-                        break;
-                    case ("previous room"):
-                        if (rooms.indexOf(player.getCurrentRoom()) != 0) {
-                            player.setCurrentRoom(rooms.get(rooms.indexOf(player.getCurrentRoom()) - 1));
-                            endRoom = true;
-                            System.out.println("Vous venez de changer de salle, observez la bien ...");
-                        } else {
-                            System.out.println("Vous êtes dans la première salle, il n'y en a pas de précédente !");
-                        }
-                        break;
-                    case ("next room"):
-                        if (player.getCurrentRoom().isUnlocked()) {
-                            if (rooms.indexOf(player.getCurrentRoom()) == rooms.size()) {
-                                player.setCurrentRoom(rooms.get(rooms.indexOf(player.getCurrentRoom()) + 1));
-                                endRoom = true;
-                                System.out.println("Vous venez de changer de salle, observez la bien ...");
-                            } else {
-                                System.out.println("Vous êtes dans la dernière salle, il n'y en a pas de suivante !");
-                            }
-                        } else {
-                            System.out.println("Un ou plusieurs virus bloquent l'accès à la salle suivante,\n" +
-                                    "faites preuve d'assurance !");
-                        }
-                        break;
-                    default:
-                        System.out.println("Ce que vous avez demandé n'est pas valide, tapez help.");
-                }
-            }
+                mainAct(rooms, characters, items);
         }
     }
 
@@ -161,5 +64,131 @@ public class Game {
     static void quit() {
         System.out.println("Au revoir !");
         System.exit(0);
+    }
+
+    static void initialize(ArrayList<Room> rooms, ArrayList<Character> characters, ArrayList<Item> items) {
+        ArrayList<Item> inventoryNull = new ArrayList<>();
+        Character player = new Character(
+                "AVAST", false, "C'est vous !", true, 1, 6, 1, inventoryNull);
+        characters.add(player);
+        Character perso1 = new Character(
+                "Antivira", false, "Antivira est un anti virus", false, 1, 6, 1, inventoryNull);
+        characters.add(perso1);
+        Character perso2 = new Character(
+                "JigSaw", true, "JigSaw est un virus méchant", false, 1, 4, 1, inventoryNull);
+        characters.add(perso2);
+        Item item0 = new Item(
+                "Bleuvrage", -2, 0, true, false, "Cet objet augmente la limite " +
+                "max des dès de 2 pour toujours !");
+        items.add(item0);
+        Item item1 = new Item(
+                "Gourde mystère", 0, -2, true, false, "Cet objet diminue la limite " +
+                "max des dès de 2 pour toujours !");
+        items.add(item1);
+
+
+        Room room0 = new Room(0, "Salle initiale", "Salle de préparation");
+        room0.getItems().add(item0);
+        room0.getItems().add(item1);
+        room0.getCharacters().add(perso1);
+        room0.getCharacters().add(perso2);
+        room0.getLockedCharacters().add(perso2);
+        rooms.add(room0);
+
+        Room room1 = new Room(1, "Salle infernale", "Description salle infernale");
+        rooms.add(room1);
+
+        player.setCurrentRoom(room0);
+    }
+
+    static void mainAct(ArrayList<Room> rooms, ArrayList<Character> characters, ArrayList<Item> items) {
+        //Ask of action from the player
+        System.out.println("Agissez !");
+
+        Scanner sc = new Scanner(System.in);
+        String wordRead = sc.nextLine();
+
+        switch (wordRead) {
+            case ("help"):
+                help();
+                break;
+            case ("quit"):
+                quit();
+                break;
+            case ("inventory"):
+                characters.get(0).manageInventory();
+                break;
+            case ("speak"):
+                manageSpeak(rooms, characters, items);
+                break;
+            case ("take"):
+                manageTake(rooms, characters, items);
+                break;
+            case ("look room"):
+                characters.get(0).getCurrentRoom().presentRoom();
+                break;
+            case ("previous room"):
+                managePreviousRoom(rooms, characters, items);
+                break;
+            case ("next room"):
+                manageNextRoom(rooms, characters, items);
+                break;
+            default:
+                System.out.println("Ce que vous avez demandé n'est pas valide, tapez help.");
+        }
+    }
+
+    static void manageSpeak(ArrayList<Room> rooms, ArrayList<Character> characters, ArrayList<Item> items){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Avec qui voulez-vous parler ?");
+        String wordRead = sc.nextLine();
+        if (Room.containCharac(wordRead, rooms.get(0).getCharacters())) {
+            if (Room.getCharac(wordRead, rooms.get(0).getCharacters()).isWicked()) {
+                System.out.println(Room.getCharac(wordRead, rooms.get(0).getCharacters()).getName() +
+                        " n'aime pas quand on lui parle..\n" +
+                        "Cela va se régler en combat !");
+                characters.get(0).fight(Room.getCharac(wordRead, rooms.get(0).getCharacters()), 2);
+            } else {
+                System.out.println(Room.getCharac(wordRead, rooms.get(0).getCharacters()).getName() +
+                        " veut vous aider mais ne sait toujours pas comment, revenez plus tard !");
+            }
+        } else {
+            System.out.println("Ce personnage n'est pas dans cette salle.");
+        }
+    }
+
+    static void manageTake(ArrayList<Room> rooms, ArrayList<Character> characters, ArrayList<Item> items){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quel objet voulez-vous récupérer ?");
+        String wordRead = sc.nextLine();
+        if (Item.containItem(wordRead, rooms.get(0).getItems())) {
+            characters.get(0).takeItem(Item.getItem(wordRead, rooms.get(0).getItems()));
+        } else {
+            System.out.println("Cet item n'est pas dans cette salle.");
+
+        }
+    }
+
+    static void managePreviousRoom(ArrayList<Room> rooms, ArrayList<Character> characters, ArrayList<Item> items){
+        if (rooms.indexOf(characters.get(0).getCurrentRoom()) != 0) {
+            characters.get(0).setCurrentRoom(rooms.get(rooms.indexOf(characters.get(0).getCurrentRoom()) - 1));
+            System.out.println("Vous venez de changer de salle, observez la bien ...");
+        } else {
+            System.out.println("Vous êtes dans la première salle, il n'y en a pas de précédente !");
+        }
+    }
+
+    static void manageNextRoom(ArrayList<Room> rooms, ArrayList<Character> characters, ArrayList<Item> items){
+        if (characters.get(0).getCurrentRoom().isUnlocked()) {
+            if (rooms.indexOf(characters.get(0).getCurrentRoom()) == rooms.size()) {
+                characters.get(0).setCurrentRoom(rooms.get(rooms.indexOf(characters.get(0).getCurrentRoom()) + 1));
+                System.out.println("Vous venez de changer de salle, observez la bien ...");
+            } else {
+                System.out.println("Vous êtes dans la dernière salle, il n'y en a pas de suivante !");
+            }
+        } else {
+            System.out.println("Un ou plusieurs virus bloquent l'accès à la salle suivante,\n" +
+                    "faites preuve d'assurance !");
+        }
     }
 }
